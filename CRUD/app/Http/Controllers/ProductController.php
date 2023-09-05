28aa87915c5cba3de3f5911a0cc5cdfaf8d0548a
Razan Mustafa <razanmustafa8@gmail.com>
2023-09-05 12:31:25 +0300

    New update

--- CRUD/app/Http/Controllers/ProductController.php
+++ CRUD/app/Http/Controllers/ProductController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products=Product::all();
+        return view('index',compact('products'));
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect(route('product.index'))->with('success, Product deleted successfully');
+    }
 }
